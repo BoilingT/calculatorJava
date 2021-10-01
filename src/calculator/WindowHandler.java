51356562a2a79ddb436dc0e9d7868274d52d9384
Boil <tobbe12a@hotmail.se>
2021-10-01 23:51:25 +0200

    Added components

--- src/calculator/WindowHandler.java
+++ src/calculator/WindowHandler.java
@@ -8,20 +8,19 @@ public class WindowHandler extends JFrame{
 	
 	public WindowHandler(int width, int height, String title) {
 		this.setTitle(title);
-		//Gör en panel så att ramen anpassar sin storlek efter panelens storlek.
-		//På så sätt får ritområdet samma dimensioner som den givna dimensionen.
+		//Gï¿½r en panel sï¿½ att ramen anpassar sin storlek efter panelens storlek.
+		//Pï¿½ sï¿½ sï¿½tt fï¿½r ritomrï¿½det samma dimensioner som den givna dimensionen.
 		//-----------------------------------------------------
 		JPanel panel = new JPanel();
 		panel.setOpaque(false);
 		panel.setPreferredSize(new Dimension(width, height));
 		//-----------------------------------------------------
 		add(panel);
-		/*Gör så att användaren inte kan ändra storleken på fönstret, 
-		 * annars måste panelen anpassa sin storlek till den nya storleken 
-		 * vilket är en funktion som jag inte vill lägga till
+		/*Gï¿½r sï¿½ att anvï¿½ndaren inte kan ï¿½ndra storleken pï¿½ fï¿½nstret, 
+		 * annars mï¿½ste panelen anpassa sin storlek till den nya storleken 
+		 * vilket ï¿½r en funktion som jag inte vill lï¿½gga till
 		 */
-		setResizable(false);
-		//Ändrar storleken på fönstret så att allting inuti fönstret för plats och syns
+		//ï¿½ndrar storleken pï¿½ fï¿½nstret sï¿½ att allting inuti fï¿½nstret fï¿½r plats och syns
 		pack();
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
--- src/calculator/WindowStructure.java
+++ src/calculator/WindowStructure.java
@@ -1,5 +1,7 @@
 package calculator;
 
+import java.awt.*;
+
 import javax.swing.*;
 
 public class WindowStructure {
@@ -11,6 +13,75 @@ public class WindowStructure {
 	
 	public void Init() {
 		
-				
+		
+		JPanel numbBtnPanel = new JPanel();
+		JButton[] numbBtns = new JButton[10];
+		
+		JButton addBtn = new JButton();
+		JButton subtractBtn = new JButton();
+		JButton divideBtn = new JButton();
+		JButton multBtn = new JButton();
+		
+		window.setLayout(new GridBagLayout());
+
+		//numbBtnPanel
+		numbBtnPanel.setLayout(new GridBagLayout());
+		GridBagConstraints numbBtnC = new GridBagConstraints();
+		numbBtnC.ipady = 50;
+		numbBtnC.ipadx = 50;
+		numbBtnC.insets = new Insets(2,2,2,2);
+		
+		//numbBtnPanel Numeric Buttons
+		int spacing = 5;
+		int total = 0;
+		for (int i = 0; i < 3; i++) {
+			for(int j = 0; j < 3; j++) {
+
+				numbBtns[i] = new JButton();
+				numbBtns[i].setName("numBtn"+(9-total++));
+				numbBtns[i].setText(String.valueOf(j));
+				numbBtnC.gridx = j;
+				numbBtnC.gridy = i+1;
+
+				numbBtnPanel.add(numbBtns[i], numbBtnC);
+			}
+		}
+		
+		numbBtns[9] = new JButton();
+		numbBtns[9].setName("numBtn0");
+		numbBtns[9].setText(String.valueOf(0));
+		numbBtnC.gridx = 2;
+		numbBtnC.gridy = 4;
+
+		numbBtnPanel.add(numbBtns[9], numbBtnC);
+		
+		//numbBtnPanel Operation Buttons
+		numbBtnC.gridx = 4;
+		
+		//Divide
+		numbBtnC.gridy = 0;
+		divideBtn.setText("/");
+		numbBtnPanel.add(divideBtn, numbBtnC);
+		
+		//Mult
+		numbBtnC.gridy = 1;
+		multBtn.setText("*");
+		numbBtnPanel.add(multBtn, numbBtnC);
+		
+		//Subtract
+		numbBtnC.gridy = 2;
+		subtractBtn.setText("-");
+		numbBtnPanel.add(subtractBtn, numbBtnC);
+		
+		//Add
+		numbBtnC.gridy = 3;
+		addBtn.setText("+");
+		numbBtnPanel.add(addBtn, numbBtnC);
+
+		numbBtnC.fill = GridBagConstraints.HORIZONTAL;
+		numbBtnC.gridx = 0;
+		numbBtnC.gridy = 2;
+		window.add(numbBtnPanel, numbBtnC);
+		window.Show();
 	}
 }
